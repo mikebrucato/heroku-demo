765645601223326a14d6f40bc41678934dc58a67
mikebrucato <brucatomike1@gmail.com>
2021-02-25 20:38:04 -0600

    add logic to select database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express");
 
 const mongoose = require("mongoose");
@@ -16,9 +17,19 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+if (process.env.NODE_ENV === "production") {
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
+} else {
+  mongoose.connect("mongodb://localhost/reactreadinglist");
+}
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
